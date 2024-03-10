b8f33c6dc516a305de8eb1b084190eaf74ac1770
Andriy Sichniy <sichniy.andriy@gmail.com>
2024-03-10 12:13:18 +0200

    change if in ch6

--- src/main/java/chapter_6/Main.java
+++ src/main/java/chapter_6/Main.java
@@ -10,10 +10,8 @@ public class Main {
     public static void main(String[] args) {
         int year = SyntaxUtil.getIntNumber("Введіть рік: ");
         String res = "Рік не високосний";
-        if (0 == year % 100) {
-            if (0 == year % 400) {
-                res = "Рік високосний";
-            }
+        if (0 == year % 100 && 0 == year % 400) {
+            res = "Рік високосний";
         } else if (0 == year % 4) {
             res = "Рік високосний";
         }
